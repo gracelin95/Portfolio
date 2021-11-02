9ca20748ed7efdcbf06e5b77f20bf87f9ead5285
Grace Lin <gracelin3295@gmail.com>
2021-11-02 16:49:34 -0400

    adjusted plane size

--- Fullstack/Async Week/Async Week Project/my-portfolio-Vite/main.js
+++ Fullstack/Async Week/Async Week Project/my-portfolio-Vite/main.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
@@ -5,15 +6,15 @@ import * as dat from "dat.gui";
 const gui = new dat.GUI();
 const world = {
   plane: {
-    width: 10,
-    height: 10,
-    widthSegments: 10,
-    heightSegments: 10,
+    width: 24,
+    height: 24,
+    widthSegments: 25,
+    heightSegments: 24,
   },
 };
-gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "width", 1, 50).onChange(generatePlane);
 
-gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 50).onChange(generatePlane);
 
 gui.add(world.plane, "widthSegments", 1, 30).onChange(generatePlane);
 
@@ -35,6 +36,15 @@ function generatePlane() {
 
     array[i + 2] = z + Math.random();
   }
+  const colors = [];
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4);
+  }
+
+  planeMesh.geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  );
 }
 const raycaster = new THREE.Raycaster();
 const scene = new THREE.Scene();
@@ -53,7 +63,12 @@ document.body.appendChild(renderer.domElement);
 new OrbitControls(camera, renderer.domElement);
 camera.position.z = 5;
 
-const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const planeGeometry = new THREE.PlaneGeometry(
+  world.plane.width,
+  world.plane.height,
+  world.plane.widthSegments,
+  world.plane.heightSegments
+);
 
 const planeMaterial = new THREE.MeshPhongMaterial({
   side: THREE.DoubleSide,
@@ -74,7 +89,7 @@ for (let i = 0; i < array.length; i += 3) {
 
 const colors = [];
 for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-  colors.push(0, 0, 1);
+  colors.push(0, 0.19, 0.4);
 }
 
 planeMesh.geometry.setAttribute(
@@ -105,20 +120,54 @@ function animate() {
   if (intersects.length > 0) {
     const { color } = intersects[0].object.geometry.attributes;
     //vertice 1
-    color.setX(intersects[0].face.a, 1);
-    color.setY(intersects[0].face.a, 0);
-    color.setZ(intersects[0].face.a, 0);
+    color.setX(intersects[0].face.a, 0.1);
+    color.setY(intersects[0].face.a, 0.5);
+    color.setZ(intersects[0].face.a, 1);
 
     //vertice 2
-    color.setX(intersects[0].face.b, 1);
-    color.setY(intersects[0].face.b, 0);
-    color.setZ(intersects[0].face.b, 0);
+    color.setX(intersects[0].face.b, 0.1);
+    color.setY(intersects[0].face.b, 0.5);
+    color.setZ(intersects[0].face.b, 1);
 
     //vertice 3
-    color.setX(intersects[0].face.c, 1);
-    color.setY(intersects[0].face.c, 0);
-    color.setZ(intersects[0].face.c, 0);
+    color.setX(intersects[0].face.c, 0.1);
+    color.setY(intersects[0].face.c, 0.5);
+    color.setZ(intersects[0].face.c, 1);
     intersects[0].object.geometry.attributes.color.needsUpdate = true;
+
+    const initialColor = {
+      r: 0,
+      g: 0.19,
+      b: 0.4,
+    };
+
+    const hoverColor = {
+      r: 0.1,
+      g: 0.5,
+      b: 1,
+    };
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      onUpdate: () => {
+        //vertice 1
+        color.setX(intersects[0].face.a, hoverColor.r);
+        color.setY(intersects[0].face.a, hoverColor.g);
+        color.setZ(intersects[0].face.a, hoverColor.b);
+
+        //vertice 2
+        color.setX(intersects[0].face.b, hoverColor.r);
+        color.setY(intersects[0].face.b, hoverColor.g);
+        color.setZ(intersects[0].face.b, hoverColor.b);
+
+        //vertice 3
+        color.setX(intersects[0].face.c, hoverColor.r);
+        color.setY(intersects[0].face.c, hoverColor.g);
+        color.setZ(intersects[0].face.c, hoverColor.b);
+        color.needsUpdate = true;
+      },
+    });
   }
 }
 
